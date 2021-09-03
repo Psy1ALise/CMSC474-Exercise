2b15e6cf632c4f5209cbce3f84d47dc641771f88
Psy1ALise <54691057+Psy1ALise@users.noreply.github.com>
2021-09-03 14:06:11 -0400

    added jQuery

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    console.log("ready");
+
+    $(".container").on("click", "#go", function() {
+        $("#jumbotron").html("Name: " + $("#fname").val() + " " + $("#lname").val());
+        return false;
+    });
+});
